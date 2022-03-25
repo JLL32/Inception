0cb3b83207ebe450e0693cfac3d740bb61e0d65a
Abdeljalil Elhachimi <eabduljalil@outlook.com>
2022-03-25 20:09:39 +0100

    rename database user to db_user in wp-config

--- srcs/requirements/wordpress/wp-config.php
+++ srcs/requirements/wordpress/wp-config.php
@@ -23,7 +23,7 @@
 define( 'DB_NAME', 'wpdb' );
 
 /** MySQL database username */
-define( 'DB_USER', 'wpuser' );
+define( 'DB_USER', 'db_user' );
 
 /** MySQL database password */
 define( 'DB_PASSWORD', '12345' );
