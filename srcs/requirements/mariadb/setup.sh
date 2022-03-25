741fd99e30754c24a4e983d34f346b0d449f3419
Abdeljalil Elhachimi <eabduljalil@outlook.com>
2022-03-25 20:03:35 +0100

    simplify mariadb setup using env vars

--- srcs/requirements/mariadb/setup.sh
+++ srcs/requirements/mariadb/setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 service mysql start 
-echo "CREATE DATABASE wpdb;"\
-	 "GRANT ALL PRIVILEGES ON wpdb.* TO 'root'@'localhost' WITH GRANT OPTION;"\
-	 "update mysql.user set plugin='mysql_native_password' where user='root';"\
-	 "FLUSH PRIVILEGES;" | mysql -u root --skip-password
 
-mysql -e "CREATE USER 'wpuser'@'%' identified by '12345';"
-mysql -e "GRANT ALL PRIVILEGES ON wpdb.* TO wpuser@'%' IDENTIFIED BY '12345'"
+mysql -e "CREATE DATABASE ${DB_NAME};"
+mysql -e "CREATE USER '${DB_USER}'@'%' identified by '${DB_PASS}';"
+mysql -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO ${DB_USER}@'%' IDENTIFIED BY '${DB_PASS}'"
 mysql -e "FLUSH PRIVILEGES;"
 
-mysql -u root --skip-password wpdb < wordpress.sql
+mysql -u root --skip-password ${DB_NAME} < wordpress.sql
+service mysql stop
+
+mysqld_safe
