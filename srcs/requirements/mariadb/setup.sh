7205beb8329e7ad95559d88be82e9ced3d4bc446
Abdeljalil Elhachimi <eabduljalil@outlook.com>
2022-03-26 03:33:58 +0100

    remove useless command

--- srcs/requirements/mariadb/setup.sh
+++ srcs/requirements/mariadb/setup.sh
@@ -4,9 +4,6 @@ service mysql start
 mysql -e "CREATE DATABASE ${DB_NAME};"
 mysql -e "CREATE USER '${DB_USER}'@'%' identified by '${DB_PASS}';"
 mysql -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO ${DB_USER}@'%' IDENTIFIED BY '${DB_PASS}'"
-mysql -e "FLUSH PRIVILEGES;"
 
 mysql -u root --skip-password ${DB_NAME} < wordpress.sql
 service mysql stop
-
-mysqld_safe
