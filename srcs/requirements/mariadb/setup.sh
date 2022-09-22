752a39ffc871bdd288f99b8cb6d0f1f9d2fb0d77
abduljalil-alhashimi-Outlier <eabduljalil@outlook.com>
2022-09-22 05:49:54 +0100

    add data persistence

--- srcs/requirements/mariadb/setup.sh
+++ srcs/requirements/mariadb/setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-service mysql start 
-
-mysql -e "CREATE DATABASE ${DB_NAME};"
-mysql -e "CREATE USER '${DB_USER}'@'%' identified by '${DB_PASS}';"
-mysql -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO ${DB_USER}@'%' IDENTIFIED BY '${DB_PASS}'"
-
-mysql -u root --skip-password ${DB_NAME} < wordpress.sql
-service mysql stop
-
+if [ ! -f "/var/lib/mysql/ibdata1" ]; then
+	mysql_install_db
+  service mysql start 
+  mysql -u root --skip-password ${DB_NAME} < wordpress.sql
+  mysql -e "CREATE DATABASE ${DB_NAME};"
+  mysql -e "CREATE USER '${DB_USER}'@'%' identified by '${DB_PASS}';"
+  mysql -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO ${DB_USER}@'%' IDENTIFIED BY '${DB_PASS}'"
+  service mysql stop
+fi
 mysqld_safe
