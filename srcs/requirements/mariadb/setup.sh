b40805ec92646af6db6422d1969f68223c3c2b53
abduljalil-alhashimi-Outlier <eabduljalil@outlook.com>
2022-09-22 06:06:22 +0100

    alter root credentials

--- srcs/requirements/mariadb/setup.sh
+++ srcs/requirements/mariadb/setup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-if [ ! -f "/var/lib/mysql/ibdata1" ]; then
+if [ ! -f "/var/lib/mysql/${DB_NAME}" ]; then
 	mysql_install_db
   service mysql start 
-  mysql -u root --skip-password ${DB_NAME} < wordpress.sql
   mysql -e "CREATE DATABASE ${DB_NAME};"
   mysql -e "CREATE USER '${DB_USER}'@'%' identified by '${DB_PASS}';"
   mysql -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO ${DB_USER}@'%' IDENTIFIED BY '${DB_PASS}'"
-  service mysql stop
+  mysql -u root --skip-password ${DB_NAME} < wordpress.sql
+
+  mysql -e "ALTER USER '${ROOT_USER}'@'localhost' IDENTIFIED BY '${ROOT_PASS}';"
+  mysql -u$ROOT_USER -p$ROOT_PASS -e "FLUSH PRIVILEGES;"
+
+  mysqladmin -u $ROOT_USER -p$ROOT_PASS shutdown
 fi
 mysqld_safe
